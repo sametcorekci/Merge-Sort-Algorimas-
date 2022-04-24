4640333318fd73d372ee2102759067ae093215a7
sametcorekci <85850750+sametcorekci@users.noreply.github.com>
2022-04-25 02:29:20 +0300

    Merge Sort Algoriması
    Merge Sort Algoriması

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Merge_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ merge_sort_algoriması.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Merge_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Proje 2
+            // [16,21,11,8,12,22] -> Merge Sort
+
+            // Yukarıdaki dizinin sort türüne göre aşamalarını yazınız.
+            // Big-O gösterimini yazınız.
+
+
+
+            // Insertion sort'da, time complexity n2 olduğundan ötürü çalışma zamanımız artıyordu. Merge sort'da ise 
+            // nlogn olduğu için açık ara performans olarak daha iyi diyebiliriz.
+
+
+        }
+    }
+}
